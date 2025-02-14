38bdc1a89a64817376a541cb26135436c08bde45
Greho5 <grzegorz0polko@gmail.com>
2025-02-14 15:47:33 +0100

    Dodaj pliki projektów.

---
+++ Cs GP Kalkulator1/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cs_GP_Kalkulator1
+{
+    public partial class Form1: Form
+    {
+        double Result = 0;
+        string number1 = "", number2="";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //this is 0 button
+        private void button10_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "0";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "1";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "2";
+        }
+       
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text += "9";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //tmp save
+            richTextBox2.Text += "+";
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            //tmp save
+            richTextBox2.Text += "+";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //tmp save
+            richTextBox2.Text += "+";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //tmp save
+            richTextBox2.Text += "+";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            //print result
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
---
+++ Cs GP Kalkulator1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Cs GP Kalkulator1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cs GP Kalkulator1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("dffccdd2-16f6-4ce3-a012-1a9a39049c0f")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
