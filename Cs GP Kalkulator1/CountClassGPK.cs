3efd11a8a3158fcf888ce237fae1a47856731dbd
Greho5 <grzegorz0polko@gmail.com>
2025-02-18 21:45:42 +0100

    Created CounClassGPK taking care for all primery counting and connected it to equation button

---
+++ Cs GP Kalkulator1/CountClassGPK.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_GP_Kalkulator1
+{
+    class CountClassGPK
+    {
+        private string[] tempTable;
+        private string temp = "", number1= "", number2="",znak="";
+        double result = 0;
+        public string countFunction(string inputNumberGP)
+        {
+            temp = inputNumberGP;
+            temp = temp.Replace("\n",String.Empty).Replace("\r", String.Empty).Replace("\t", String.Empty);
+            Console.WriteLine(temp);
+            Console.WriteLine("####");
+            if (inputNumberGP.Contains("+"))
+            {
+                znak = "+";
+                tempTable = temp.Split('+');
+                number1 = tempTable[0];
+                number2 = tempTable[1];
+                result = Convert.ToDouble(number1) + Convert.ToDouble(number2);
+            }
+            else if (inputNumberGP.Contains("-"))
+            {
+                znak = "-";
+                tempTable = temp.Split('-');
+                number1 = tempTable[0];
+                number2 = tempTable[1];
+                result = Convert.ToDouble(number1) - Convert.ToDouble(number2);
+            }
+            else if (inputNumberGP.Contains("*"))
+            {
+                znak = "*";
+                tempTable = temp.Split('*');
+                number1 = tempTable[0];
+                number2 = tempTable[1];
+                result = Convert.ToDouble(number1) * Convert.ToDouble(number2);
+            }
+            else if (inputNumberGP.Contains("/"))
+            {
+                znak = "/";
+                tempTable = temp.Split('/');
+                number1 = tempTable[0];
+                number2 = tempTable[1];
+                result = Convert.ToDouble(number1) / Convert.ToDouble(number2);
+            }
+            Console.WriteLine(number1 +" _ "+ number2 +" _ "+ znak +" _ "+ result);
+            return (number1 + Environment.NewLine + znak + Environment.NewLine + number2 + Environment.NewLine + result.ToString());
+        }
+
+    }
+}
--- Cs GP Kalkulator1/Form1.cs
+++ Cs GP Kalkulator1/Form1.cs
@@ -7,13 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static Cs_GP_Kalkulator1.CountClassGPK;
 
 namespace Cs_GP_Kalkulator1
 {
     public partial class Form1: Form
     {
         double Result = 0;
-        string number1 = "", number2="";
+        string number1 = "", number2="",numbertmp= "",strresult="";
+        Boolean numb1bool = false, numb2bool = false;
+        CountClassGPK countClassGPinstance = new CountClassGPK();
         public Form1()
         {
             InitializeComponent();
@@ -75,32 +78,60 @@ namespace Cs_GP_Kalkulator1
 
         private void button11_Click(object sender, EventArgs e)
         {
-            //tmp save
-            richTextBox2.Text += "+";
-
+            //add tmp save
+            number1 = richTextBox2.Text;
+            richTextBox2.Text += $"{Environment.NewLine}+{Environment.NewLine}";
+            numbertmp = richTextBox2.Text;
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
             //tmp save
-            richTextBox2.Text += "+";
+            number1 = richTextBox2.Text;
+            richTextBox2.Text += $"{Environment.NewLine}-{Environment.NewLine}";
+            numbertmp = richTextBox2.Text;
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
             //tmp save
-            richTextBox2.Text += "+";
+            number1 = richTextBox2.Text;
+            richTextBox2.Text += $"{Environment.NewLine}*{Environment.NewLine}";
+            numbertmp = richTextBox2.Text;
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
             //tmp save
-            richTextBox2.Text += "+";
+            number1 = richTextBox2.Text;
+            richTextBox2.Text += $"{Environment.NewLine}/{Environment.NewLine}";
+            numbertmp = richTextBox2.Text;
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
             //print result
+            //number2 = string.Expect(richTextBox2.Text,numbertmp);
+
+
+            //countClassGPinstance.countFunction(richTextBox2.Text);
+
+            // to delete inicial try to get result without use of class
+            /* List<string> set1 = richTextBox2.Text.Split(' ').Distinct().ToList();
+            List<string> set2 = numbertmp.Split(' ').Distinct().ToList();
+
+            var diff = set2.Count() > set1.Count() ? set2.Except(set1).ToList() : set1.Except(set2).ToList();
+            number2 = string.Join("", diff);
+            Console.WriteLine(number1+" "+number2);*/
+
+            //Result = Convert.ToDouble(number1) + Convert.ToDouble(number2);
+            //richTextBox2.Text = Result.ToString();
+
+
+            //count every thing in class fuction end return result string
+            strresult = countClassGPinstance.countFunction(richTextBox2.Text);
+            richTextBox2.Text = "";
+            richTextBox2.Text = strresult;
         }
 
         private void richTextBox2_TextChanged(object sender, EventArgs e)
